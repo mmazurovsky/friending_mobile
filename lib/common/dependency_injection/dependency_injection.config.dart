b288d0cafacb12836f6052d889e909c9a8bc2370
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2023-03-03 14:14:23 +0100

    - minor

--- lib/common/dependency_injection/dependency_injection.config.dart
+++ lib/common/dependency_injection/dependency_injection.config.dart
@@ -18,7 +18,7 @@ import 'package:flutter_mobile_starter/common/utils/logger/logger_config.dart'
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'module.dart' as _i10;
+import 'modules.dart' as _i10;
 
 // ignore_for_file: unnecessary_lambdas
 // ignore_for_file: lines_longer_than_80_chars
